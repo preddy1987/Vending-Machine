57dc1a6e31d23a0dbf83c3a74f7a0e453bda4110
ascensionmartinez <atm42486@gmail.com>
2019-03-21 16:10:05 -0400

    added buttons

--- Web/js/user.js
+++ Web/js/user.js
@@ -4,29 +4,42 @@ const g_main = document.querySelector('main');
 function displayUsers(){
     const displayButton = document.createElement('button'); 
     displayButton.innerText = "Display Users";
-    displayButton.setAttribute('id','display-btn');
-    g_main.insertAdjacentElement('beforeend',displayButton);
+    const g_divContainer = document.createElement('div');
+    g_divContainer.setAttribute('id','display-btn');
+    g_main.insertAdjacentElement('beforeend', g_divContainer);
+    const buttonDiv = document.querySelector('main>div');
+    buttonDiv.insertAdjacentElement('beforeend',displayButton);
 }
 
 function addUser(){
     const addButton = document.createElement('button'); 
     addButton.innerText = "Add User";
-    addButton.setAttribute('id','add-btn');
-    g_main.insertAdjacentElement('beforeend',addButton);
+    
+    const g_divContainer = document.createElement('div');
+    g_divContainer.setAttribute('id','add-btn');
+    g_main.insertAdjacentElement('beforeend', g_divContainer);
+    const buttonDiv = document.querySelector('main :nth-child(2)');
+    buttonDiv.insertAdjacentElement('beforeend',addButton);
 }
 
 function updateUser(){
     const updateButton = document.createElement('button'); 
     updateButton.innerText = "Update User";
-    updateButton.setAttribute('id','update-btn');
-    g_main.insertAdjacentElement('beforeend',updateButton);
+    const g_divContainer = document.createElement('div');
+    g_divContainer.setAttribute('id','update-btn');
+    g_main.insertAdjacentElement('beforeend', g_divContainer);
+    const buttonDiv = document.querySelector('main :nth-child(3)');
+    buttonDiv.insertAdjacentElement('beforeend',updateButton);
 }
 
 function deleteUser(){
     const deleteButton = document.createElement('button'); 
     deleteButton.innerText = "Delete User";
-    deleteButton.setAttribute('id','delete-btn');
-    g_main.insertAdjacentElement('beforeend',deleteButton);
+    const g_divContainer = document.createElement('div');
+    g_divContainer.setAttribute('id','delete-btn');
+    g_main.insertAdjacentElement('beforeend', g_divContainer);
+    const buttonDiv = document.querySelector('main :nth-child(4)');
+    buttonDiv.insertAdjacentElement('beforeend',deleteButton);
 }
 
 
@@ -40,28 +53,55 @@ document.addEventListener('DOMContentLoaded', () => {
     let displayNode = document.getElementById('display-btn');
     displayNode.addEventListener('click', () => {
       fetch('http://localhost:57005/api/user')
-    //   , { 
-    //   mode: 'cors',
-    //   headers:{
-    //     'Access-Control-Allow-Origin':'*'
-    //     },
-    //    }) 
        .then((response) => {
           return response.json();
       })
       .then((data) => {
 
         console.log(data);
-        //   let output = '<div></div>';
-        //   data.forEach(element => {
-        //       output +=`
-        //       <h1>test</h1>
-        //       <p>${element.firstname}</p>
-        //       `;
-        //   });
-        //   g_main.insertAdjacentElement('afterend',output);
+          let output = '';
+          data.forEach(element => {
+              output +=`
+              <div>
+              <p>${element.firstName}</p>
+              <p>${element.lastName}</p>
+              <p>${element.username}</p>
+              <p>${element.email}</p>
+              <p>${element.roleId}</p>
+              </div>
+              `;
+          });
+          g_main.insertAdjacentHTML('beforeend',output);
       })
     })
+
+    // let displayNode = document.getElementById('add-btn');
+    // displayNode.addEventListener('click', () => {
+    //   fetch('http://localhost:57005/api/user', {
+    //     method: 'Post'
+    //   }).then((response) => {
+    //       return response.json();
+    //   })
+    //   .then((data) => {
+
+    //     console.log(data);
+    //       let output = '';
+    //       data.forEach(element => {
+    //           output +=`
+    //           <div>
+    //           <p>${element.firstName}</p>
+    //           <p>${element.lastName}</p>
+    //           <p>${element.username}</p>
+    //           <p>${element.email}</p>
+    //           <p>${element.roleId}</p>
+    //           </div>
+    //           `;
+    //       });
+    //       g_main.insertAdjacentHTML('beforeend',output);
+    //   })
+    // })
+
+
 
   });
 

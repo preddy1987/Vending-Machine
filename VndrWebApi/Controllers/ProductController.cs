44f8740613398474fcee20a304fe0779d9a50f39
patrickreddy <patrickreddy87@gmail.com>
2019-03-21 11:57:05 -0400

    finished ProductApi

--- VndrWebApi/Controllers/ProductController.cs
+++ VndrWebApi/Controllers/ProductController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VendingService.Interfaces;
+using VendingService.Models;
+using VndrWebApi.Models;
 
 namespace VndrWebApi.Controllers
 {
@@ -11,5 +14,57 @@ namespace VndrWebApi.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private IVendingService _db = null;
+
+        public ProductController(IVendingService db)
+        {
+            _db = db;
+        }
+
+        // GET api/role
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductItem>> Get()
+        {
+            return _db.GetProductItems();
+        }
+
+        // GET api/role/5
+        [HttpGet("{id}")]
+        public ActionResult<ProductItem> Get(int id)
+        {
+            return _db.GetProductItem(id);
+        }
+
+        // POST api/role
+        [HttpPost]
+        public void Post([FromBody] ProductItemViewModel value)
+        {
+            ProductItem item = new ProductItem();
+            item.Name = value.Name;
+            item.CategoryId = value.CategoryId;
+            item.Price = value.Price;
+            item.Image = value.Image;
+            _db.AddProductItem(item);
+        }
+
+        // PUT api/role/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] ProductItemViewModel value)
+        {
+            ProductItem item = new ProductItem();
+            item.Id = id;
+            item.Name = value.Name;
+            item.CategoryId = value.CategoryId;
+            item.Price = value.Price;
+            item.Image = value.Image;
+            _db.UpdateProductItem(item);
+        }
+
+        // DELETE api/role/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _db.DeleteProductItem(id);
+        }
     }
 }

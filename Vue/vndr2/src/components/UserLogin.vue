eb94ae19dc365d224bd4aff38bb13f40b3309461
ascensionmartinez <atm42486@gmail.com>
2019-03-29 15:37:59 -0400

    added user inputs

---
+++ Vue/vndr2/src/components/UserLogin.vue
@@ -0,0 +1,51 @@
+<template>
+<div id="user-login">
+    <form>
+        <div>
+        <label for="username">Username:</label>
+        <input type="text" class="form-control" name="username" id="username" >
+        </div>
+        <div>
+        <label for="password">Password:</label>
+        <input type="text" class="form-control" name="password" id="password">
+        </div>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+ name: "user",
+    data() {
+        return {
+            // firstName: '',
+            // lastName: '',
+            // username: '',
+            // email: '',
+            // password: '',
+            // roleId: 0,
+            // userId: 0
+            username: '',
+            password: ''
+        }
+    }
+}
+</script>
+
+<style>
+.form-control{
+    max-width: 40%;
+    margin: 20px, 0px, 20px, 0px,
+}
+
+#user-login form{
+    text-align: center;
+}
+#login-user{
+    max-width: 40%;
+    margin-top: 10px;
+}
+#add-user{
+    margin-top: 10px;
+}
+</style>
